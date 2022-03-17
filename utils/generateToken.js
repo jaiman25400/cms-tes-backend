173855e4e2be1f17adbb8bef16603cba43b7e8ae
jaiman25400 <jaimanjoshi25@gmail.com>
2022-03-17 15:23:17 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
-
+var util= require('util');
+const utf8Encoder = new util.TextEncoder();
+const utf8Decoder = new util.TextEncoder("utf-8", { ignoreBOM: true });
 const generateToken = (id) => {
     return jwt.sign({ id } , "MyOwnHeadlessCMS", { expiresIn: '30d'} )
 }
